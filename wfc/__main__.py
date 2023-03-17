f2d9295db7f9665774a3aa1e90d94c0bd65951a3
bobbarnes1981 <robertbarnes1981@googlemail.com>
2023-03-17 08:17:29 +0000

    compact rule definition

--- wfc/__main__.py
+++ wfc/__main__.py
@@ -31,114 +31,33 @@ if __name__ == '__main__':
     blank_connect_s = ['none','nonea','noneb','ew','nw','ne','new','nhouse','nwa','nea','nwall','ewall','wwall','ewwall']
     blank_connect_w = ['none','nonea','noneb','ns','se','ne','nes','nhouse','nea','sea','nwall','ewall','swall','nswall']
     tiles = {
-        'nesw': {
-            'rules': { 'n': road_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': road_connect_w },
-            'weight': 1
-        },
-        'neswew': {
-            'rules':  { 'n': road_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': road_connect_w },
-            'weight': 0.5
-        },
-        'neswns': {
-            'rules':  { 'n': road_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': road_connect_w },
-            'weight': 0.5
-        },
-        'ew': {
-            'rules':  { 'n': blank_connect_n, 'e': road_connect_e, 's': blank_connect_s, 'w': road_connect_w },
-            'weight': 1
-        },
-        'ns': {
-            'rules':  { 'n': road_connect_n, 'e': blank_connect_e, 's': road_connect_s, 'w': blank_connect_w },
-            'weight': 1
-        },
-        'none': {
-            'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': blank_connect_s, 'w': blank_connect_w },
-            'weight': 1
-        },
-        'nonea': {
-            'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': blank_connect_s, 'w': blank_connect_w },
-            'weight': 0.25
-        },
-        'noneb': {
-            'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': blank_connect_s, 'w': blank_connect_w },
-            'weight': 0.25
-        },
-        'se': {
-            'rules':  { 'n': road_connect_n, 'e': blank_connect_e, 's': blank_connect_s, 'w': road_connect_w },
-            'weight': 1
-        },
-        'sw': {
-            'rules':  { 'n': road_connect_n, 'e': road_connect_e, 's': blank_connect_s, 'w': blank_connect_w },
-            'weight': 1
-        },
-        'nw': {
-            'rules': { 'n': blank_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': blank_connect_w },
-            'weight': 1
-        },
-        'ne': {
-            'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': road_connect_s, 'w': road_connect_w },
-            'weight': 1
-        },
-        'new': {
-            'rules':  { 'n': blank_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': road_connect_w },
-            'weight': 1
-        },
-        'sew': {
-            'rules':  { 'n': road_connect_n, 'e': road_connect_e, 's': blank_connect_s, 'w': road_connect_w },
-            'weight': 1
-        },
-        'nes': {
-            'rules':  { 'n': road_connect_n, 'e': blank_connect_e, 's': road_connect_s, 'w': road_connect_w },
-            'weight': 1
-        },
-        'nws': {
-            'rules':  { 'n': road_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': blank_connect_w },
-            'weight': 1
-        },
-        'nhouse': {
-            'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': road_connect_s, 'w': blank_connect_w },
-            'weight': 2
-        },
-        'sea': {
-            'rules':  { 'n': ['nea'], 'e': blank_connect_e, 's': blank_connect_s, 'w': ['swa'] },
-            'weight': 1
-        },
-        'swa': {
-            'rules':  { 'n': ['nwa'], 'e': ['sea'], 's': blank_connect_s, 'w': blank_connect_w },
-            'weight': 1
-        },
-        'nwa': {
-            'rules':  { 'n': blank_connect_n, 'e': ['nea'], 's': ['swa'], 'w': blank_connect_w },
-            'weight': 1
-        },
-        'nea': {
-            'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': ['sea'], 'w': ['nwa'] },
-            'weight': 1
-        },
-        'nwall': {
-            'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': ['swall','nswall'], 'w': blank_connect_w },
-            'weight': 0.25
-        },
-        'ewall': {
-            'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': blank_connect_s, 'w': ['wwall','ewwall'] },
-            'weight': 0.25
-        },
-        'swall': {
-            'rules':  { 'n': ['nwall','nswall'], 'e': blank_connect_e, 's': blank_connect_s, 'w': blank_connect_w },
-            'weight': 0.25
-        },
-        'wwall': {
-            'rules':  { 'n': blank_connect_n, 'e': ['ewall','ewwall'], 's': blank_connect_s, 'w': blank_connect_w },
-            'weight': 0.25
-        },
-        'nswall': {
-            'rules':  { 'n': ['nwall','nswall'], 'e': blank_connect_e, 's': ['swall','nswall'], 'w': blank_connect_w },
-            'weight': 1
-        },
-        'ewwall': {
-            'rules':  { 'n': blank_connect_n, 'e': ['ewall','ewwall'], 's': blank_connect_s, 'w': ['wwall','ewwall'] },
-            'weight': 1
-        }
+        'nesw': { 'rules': { 'n': road_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': road_connect_w }, 'weight': 1 },
+        'neswew': { 'rules':  { 'n': road_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': road_connect_w }, 'weight': 0.5 },
+        'neswns': { 'rules':  { 'n': road_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': road_connect_w }, 'weight': 0.5 },
+        'ew': { 'rules':  { 'n': blank_connect_n, 'e': road_connect_e, 's': blank_connect_s, 'w': road_connect_w }, 'weight': 1 },
+        'ns': { 'rules':  { 'n': road_connect_n, 'e': blank_connect_e, 's': road_connect_s, 'w': blank_connect_w }, 'weight': 1 },
+        'none': { 'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': blank_connect_s, 'w': blank_connect_w }, 'weight': 1 },
+        'nonea': { 'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': blank_connect_s, 'w': blank_connect_w }, 'weight': 0.25 },
+        'noneb': { 'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': blank_connect_s, 'w': blank_connect_w }, 'weight': 0.25 },
+        'se': { 'rules':  { 'n': road_connect_n, 'e': blank_connect_e, 's': blank_connect_s, 'w': road_connect_w }, 'weight': 1 },
+        'sw': { 'rules':  { 'n': road_connect_n, 'e': road_connect_e, 's': blank_connect_s, 'w': blank_connect_w }, 'weight': 1 },
+        'nw': { 'rules': { 'n': blank_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': blank_connect_w }, 'weight': 1 },
+        'ne': { 'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': road_connect_s, 'w': road_connect_w }, 'weight': 1 },
+        'new': { 'rules':  { 'n': blank_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': road_connect_w }, 'weight': 1 },
+        'sew': { 'rules':  { 'n': road_connect_n, 'e': road_connect_e, 's': blank_connect_s, 'w': road_connect_w }, 'weight': 1 },
+        'nes': { 'rules':  { 'n': road_connect_n, 'e': blank_connect_e, 's': road_connect_s, 'w': road_connect_w },  },
+        'nws': { 'rules':  { 'n': road_connect_n, 'e': road_connect_e, 's': road_connect_s, 'w': blank_connect_w }, 'weight': 1 },
+        'nhouse': { 'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': road_connect_s, 'w': blank_connect_w }, 'weight': 2 },
+        'sea': { 'rules':  { 'n': ['nea'], 'e': blank_connect_e, 's': blank_connect_s, 'w': ['swa'] }, 'weight': 1 },
+        'swa': { 'rules':  { 'n': ['nwa'], 'e': ['sea'], 's': blank_connect_s, 'w': blank_connect_w }, 'weight': 1 },
+        'nwa': { 'rules':  { 'n': blank_connect_n, 'e': ['nea'], 's': ['swa'], 'w': blank_connect_w }, 'weight': 1 },
+        'nea': { 'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': ['sea'], 'w': ['nwa'] }, 'weight': 1 },
+        'nwall': { 'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': ['swall','nswall'], 'w': blank_connect_w }, 'weight': 0.25 },
+        'ewall': { 'rules':  { 'n': blank_connect_n, 'e': blank_connect_e, 's': blank_connect_s, 'w': ['wwall','ewwall'] }, 'weight': 0.25 },
+        'swall': { 'rules':  { 'n': ['nwall','nswall'], 'e': blank_connect_e, 's': blank_connect_s, 'w': blank_connect_w }, 'weight': 0.25 },
+        'wwall': { 'rules':  { 'n': blank_connect_n, 'e': ['ewall','ewwall'], 's': blank_connect_s, 'w': blank_connect_w }, 'weight': 0.25 },
+        'nswall': { 'rules':  { 'n': ['nwall','nswall'], 'e': blank_connect_e, 's': ['swall','nswall'], 'w': blank_connect_w }, 'weight': 1 },
+        'ewwall': { 'rules':  { 'n': blank_connect_n, 'e': ['ewall','ewwall'], 's': blank_connect_s, 'w': ['wwall','ewwall'] }, 'weight': 1 }
     }
     tileset = solver.TileSet(tiles)
 
